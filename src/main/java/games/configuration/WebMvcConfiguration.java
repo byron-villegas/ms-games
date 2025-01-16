a869ae77409b0410a4edb50b4933120e1bf0611f
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-01-15 22:04:06 -0300

    Added: WebMvcConfig and HttpInterceptor

--- src/main/java/games/configuration/WebMvcConfiguration.java
+++ src/main/java/games/configuration/WebMvcConfiguration.java
@@ -1,16 +1,17 @@
 package games.configuration;
 
+import games.interceptor.HttpInterceptor;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@AllArgsConstructor
 public class WebMvcConfiguration implements WebMvcConfigurer {
+    private final HttpInterceptor httpInterceptor;
 
     public void addInterceptors(InterceptorRegistry interceptorRegistry) {
-        interceptorRegistry.addInterceptor()
+        interceptorRegistry.addInterceptor(httpInterceptor);
     }
-
-    class Http
 }
